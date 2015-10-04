922371964b22aa72f92ab77e3eebfff6517b8046
isaacrael <grael@comcast.net>
2015-10-04 10:48:46 -0600

    git commit -m "refactored changed app name, text in index.html, and app name in urls.py"

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'travel_cost_estimator.views.index', name='index'),
+    url(r'^$', 'travel_expense_estimator.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
