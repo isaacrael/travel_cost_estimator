d5a1d300bd2c75d48b6ad2781bc4918c9b48ff66
isaacrael <grael@comcast.net>
2015-10-05 17:05:42 -0600

    added the account url

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'travel_expense_estimator.views.index', name='index'),
+    url(r'^account/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 ]
