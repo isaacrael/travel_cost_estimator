728142025aec10c31b55b101423d591690be94b6
grael@comcast.net <cjgtsm79!>
2015-10-07 11:23:53 -0600

    experimented with e-mail settings for work

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,9 +111,15 @@ LOGIN_REDIRECT_URL = '/estimator/'
 LOGOUT_REDIRECT_URL = '/account/logout'
 
 # Email Backend
+# The setting below work from Verizon hotspot and home matrix network
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "mail.gilrael.com"
 EMAIL_HOST_USER = "gil@gilrael.com"
-EMAIL_PORT = 587
+EMAIL_PORT = 143
 EMAIL_USE_TLS = True
+
+# Settings for work currently testing they do not work now
+#EMAIL_PORT = 143
+#EMAIL_USE_SSL = True
+
 DEFAULT_FROM_EMAIL = "gil@gilrael.com"
